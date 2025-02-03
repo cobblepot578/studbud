e01b40d8123d7ffdabddd478c859489b4127e931
cobblepot578 <law25503@gmail.com>
2025-02-03 11:03:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
     console.log("Study Buddy site is ready!");
+
+    const elementsToAnimate = document.querySelectorAll('.step, .animated-paragraph'); // Combine selectors
+
+    window.addEventListener('scroll', () => {
+        elementsToAnimate.forEach(element => {
+            const rect = element.getBoundingClientRect();
+            const viewportHeight = window.innerHeight;
+
+            if (rect.top < viewportHeight * 0.8) {
+                element.classList.add('animate');
+            } else {
+                element.classList.remove('animate');
+            }
+        });
+    });
 });
